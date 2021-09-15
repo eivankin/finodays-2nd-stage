76e1813801a733586d2d768464e1ef37ed918c0b
Evgenij Ivankin <e.ivankin@innopolis.university>
2021-09-16 00:36:09 +0300

    optimize configs and add params to preprocess script

--- preprocess.py
+++ preprocess.py
@@ -11,9 +11,10 @@ from sklearn.model_selection import train_test_split
 def save_data(path: Path, data: list) -> None:
     db = DocBin()
     print('Converting...')
-    for row in tqdm(data):
-        doc = nlp(row['review'].replace('<br />', '\n\n'))
-        doc.cats = {'pos': int(row['sentiment']), 'neg': 1 - int(row['sentiment'])}
+    for i, (doc) in enumerate(nlp.pipe(tqdm([r['review'].replace('<br />', '\n\n') for r in data]),
+                                       n_process=args.n_process)):
+        sentiment = int(data[i]['sentiment'])
+        doc.cats = {'pos': sentiment, 'neg': 1 - sentiment}
         db.add(doc)
     if not path.parent.exists():
         os.mkdir(path.parent)
@@ -21,17 +22,21 @@ def save_data(path: Path, data: list) -> None:
     db.to_disk(path)
 
 
+LANGUAGES = {'en': 'en_core_web_lg', 'ru': 'ru_core_news_lg'}
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--language', '-l', default='en', choices=['en', 'ru'])
+    parser.add_argument('--language', '-l', default='en', choices=list(LANGUAGES))
     parser.add_argument('--input', '-i', default='data/movie_data.csv', type=Path)
     parser.add_argument('--train-output', '-to', default='data/train/train.spacy', type=Path)
     parser.add_argument('--dev-output', '-do', default='data/dev/test.spacy', type=Path)
     parser.add_argument('--test-size', '-ts', type=float, default=0.5)
     parser.add_argument('--dataset-truncate', '-dt', type=int, default=10_000)
+    parser.add_argument('--n-process', '-np', type=int, default=4)
     args = parser.parse_args()
 
-    nlp = spacy.blank(args.language)
+    nlp = spacy.load(LANGUAGES[args.language])
+
     with open(args.input, encoding='utf8') as inp:
         all_data = list(csv.DictReader(inp))[:args.dataset_truncate]
         train, test = train_test_split(all_data, test_size=args.test_size)
